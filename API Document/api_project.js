e5670fea8d67deabdd0878c1cd089ba0695c166c
moza-loza <61754531+moza-loza@users.noreply.github.com>
2021-04-21 04:37:35 +0200

    ER Diagram and API Document

---
+++ API Document/api_project.js
@@ -0,0 +1,14 @@
+define({
+  "name": "",
+  "version": "0.0.0",
+  "description": "",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2021-04-21T01:55:44.833Z",
+    "url": "https://apidocjs.com",
+    "version": "0.27.1"
+  }
+});
