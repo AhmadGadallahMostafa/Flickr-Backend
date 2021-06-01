494ee075741ba8a7cf485d62283aeff510e199ff
moza-loza <61754531+moza-loza@users.noreply.github.com>
2021-06-01 04:52:05 +0200

    media relocation

--- api/routes/photos.js
+++ api/routes/photos.js
@@ -11,7 +11,7 @@ const multer = require("multer");
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        const dir = path.join(__dirname, "../../photos");
+        const dir = path.join(__dirname, "../../../media");
         if(!fs.existsSync(dir)) {
             fs.mkdirSync(dir);
             cb(null, dir);
